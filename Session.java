aa4b9e078893d4a698ef0def2fb1bfaec3b10b5c
MH Farahmand <mhfarahmandm@gmail.com>
2024-07-05 11:24:31 +0330

    added session

---
+++ Session.java
@@ -0,0 +1,32 @@
+public class Session {
+    private static Session instance;
+
+    private String current_menu;
+    private int logged_user_id;
+
+    private Session() {}
+
+    public static synchronized Session getInstance() {
+        if (instance == null) {
+            instance = new Session();
+        }
+        return instance;
+    }
+
+    public String getCurrentMenu() {
+        return current_menu;
+    }
+
+    public void setCurrentMenu(String current_menu) {
+        this.current_menu = current_menu;
+    }
+
+    public int getLoggedUser() {
+        return logged_user_id;
+    }
+
+    public void setLoggedUser(int logged_user) {
+        this.logged_user_id = logged_user;
+    }
+
+}
