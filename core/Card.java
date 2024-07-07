38acc838cf94d8a7bb74af0f6090d52b3201bd7c
AS_Anghaei <amirsaeedanghei@gmail.com>
2024-07-07 13:00:31 +0330

    add Card constructor

--- core/Card.java
+++ core/Card.java
@@ -1,5 +1,27 @@
 package core;  
 // this class saves all properties of a card 
 public class Card {
-    
+    private String id;
+    private String name;
+    private int duration;
+    private int defence;
+    private int damage;
+    private String explanation;
+    private String type;
+    private int level;
+    private String group;
+    //private String img;
+
+
+    public Card(String id, String name, int duration, int defence, int damage, String explanation, String type, int level, String group) {
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+        this.defence = defence;
+        this.damage = damage;
+        this.explanation = explanation;
+        this.type = type;
+        this.level = level;
+        this.group = group;
+    }
 }
