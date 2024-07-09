d0f80976befa7aa96d8e8c2baea1af233b92a292
AS_Anghaei <amirsaeedanghei@gmail.com>
2024-07-09 11:38:13 +0330

    add

--- core/Spells.java
+++ core/Spells.java
@@ -1,7 +1,18 @@
 package core;
 
 public enum Spells {
-    SHIELD("17");
+    STAR_DESTROYER("16"),
+    SHIELD("17"),
+    HEAL("18"),
+    CLOVER("19"),
+    POISON("20"),
+    FIXER("21"),
+    ALARM("22"),
+    HOLE("23"),
+    THIEF("24"),
+    SWAMP("25"),
+    CLONE("26"),
+    HIDDEN("27");
     
     private final String pattern;
 
