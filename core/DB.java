abc6660f8208752ea70063d28e60376a3777886b
Mohammad Hossein Farahmand <44579251+mhfmobin@users.noreply.github.com>
2024-07-11 17:42:16 +0330

    Update DB.java

--- core/DB.java
+++ core/DB.java
@@ -296,11 +296,9 @@ public class DB {
         command(sql);
     }
 
-    public static void createUser(String username, String password, int security_question_id,
-            String security_question_answer, String email) {
-        String sql = "INSERT INTO `users` (`id`, `username`, `password`, `security_question_id`, `security_question_answer`, `email`, `nickname`, `xp`, `coins`, `clan_id`) VALUES";
-        sql += "(NULL, '" + username + "', '" + password + "' , '" + security_question_id + "', '"
-                + security_question_answer + "', '" + email + "', '0', '0', '0', NULL)";
+    public static void createUser(String username, String password, int security_question_id, String security_question_answer, String email) {
+        String sql = "INSERT INTO `users` (`id`, `username`, `password`, `security_question_id`, `security_question_answer`, `email`, `nickname`, `xp`, `coins`, `clan_id`, `last_failed_login`, `login_fail_number`) VALUES";
+        sql += "(NULL, '" + username + "', '" + password + "', '" + security_question_id + "', '" + security_question_answer + "', '" + email + "', '" + username + "', '0', '0', '0', '0', '0')";
         command(sql);
     }
 
