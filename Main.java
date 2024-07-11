c0300256ed884830837aef808763f342cfc428c2
hsadr579 <hsadr579@gmail.com>
2024-07-11 18:48:47 +0330

    i

--- Main.java
+++
@@ -1,9 +0,0 @@
-import core.DB;
-
-public class Main {
-    public static void main(String[] args) {
-        DB.createTables();
-        Console console = new Console();
-        console.run();
-    }
-}
